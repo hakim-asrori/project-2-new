f8124d89388e377305b10a6d5726a87d0379c47f
Rantika <ranrantika40@gmail.com>
2021-11-30 14:22:02 +0700

    Edit form telepon

--- resources/views/modal/telepon.blade.php
+++ resources/views/modal/telepon.blade.php
@@ -7,8 +7,8 @@
 
                     <button type="button" class="close modal-close" data-dismiss="modal">&times;</button>
 
-                    <!-- <form action="{{ url('/auth/telepon') }}" method="post"> -->
-                    <form action="/auth/telepon" method="post">
+                    {{-- <!-- <form action="{{ url('/auth/telepon') }}" method="post"> --> --}}
+
                         @csrf
                         <div class="form-group">
                             <label>Masukan No. WA</label>
@@ -16,9 +16,9 @@
                         </div>
 
                         <div class="form-group">
-                            <button class="btn btn-primary" type="submit">Simpan</button>
+                            <button class="btn btn-primary" type="submit" id="simpanTelepon">Simpan</button>
                         </div>
-                    </form>
+
 
                 </div>
             </div>
